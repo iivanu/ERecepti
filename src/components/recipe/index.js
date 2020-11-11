d143904465ff376ea7563d114c0993d05dc8c285
Ivan Ivanusic <ivan.ivanusic@hotmail.com>
2020-11-11 15:01:51 +0100

    Added picture box in recipe

--- src/components/recipe/index.js
+++ src/components/recipe/index.js
@@ -5,6 +5,8 @@ import Styles from './style.module.css'
 const Recipe = () => {
     return (
         <div className={Styles.recipe}>
+            <div className={Styles.picture}>
+            </div>
         </div>
     )
 }
